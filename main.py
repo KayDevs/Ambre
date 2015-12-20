b59cb9f3cff0d19983b53055dc126816f2c6ff0e
Keller Hood <kellboy9@gmail.com>
2015-12-20 01:38:41 -0600

    Basic shaders

--- main.py
+++ main.py
@@ -3,31 +3,45 @@ import pyglet
 from pyglet.window import *
 from pyglet.gl import *
 from Mesh import *
+from Material import *
+
+def vec(*floats):
+	return (GLfloat * len(floats))(*floats)
 
 window = pyglet.window.Window()
 
 glEnable(GL_DEPTH_TEST)
 glDepthFunc(GL_LESS)
+glEnable(GL_CULL_FACE)
+glCullFace(GL_BACK)
 glEnableClientState(GL_VERTEX_ARRAY)
 
 vertices = [
 0.0, 0.0, 0.0,
 window.width, 0.0, 0.0,
-window.width, window.height, 0.0
+window.width, window.height, -2.0
 ]
+
+vs = open('./basic_vs.glsl')
+vs_lines = vs.readlines()
+fs = open('./basic_fs.glsl')
+fs_lines = fs.readlines()
 
 newMesh = Mesh()
 newMesh.load(vertices)
+newMaterial = Material(vs_lines, fs_lines)
 
 @window.event
 def on_draw():
 	window.clear()
+	if(newMaterial.linked):
+		newMaterial.bind()
 	newMesh.draw()
 
 @window.event
 def on_key_press(symbol, modifiers):
-    if symbol == key.A:
-		print "left"
+	if symbol == key.SPACE:
+		print "cool"
 
 @window.event
 def on_mouse_press(x, y, button, modifiers):
